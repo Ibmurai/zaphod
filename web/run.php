b8d98e85f5bdc48a7784a1010c36e857f1db339a
Bo Thinggaard <bo@tnactas.dk>
2011-12-22 14:38:03 +0100

    fixed module path

--- web/run.php
+++ web/run.php
@@ -1,5 +1,4 @@
 <?php
 require_once(__DIR__ . '/../lib/frood/src/Frood.php');
-$frood = new Frood('Lolmodule', 'public');
-//optional config here $frood->configure();
+$frood = new Frood();
 $frood->dispatch();
