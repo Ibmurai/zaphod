50620ede710513768eb3c7ac048ef1730f4707dc
Jens Riisom Schultz <ibber_of_crew42@hotmail.com>
2011-12-22 12:27:19 +0100

    Added stuff. Stuff that was missing. Oh, and modified other stuff.

--- web/run.php
+++ web/run.php
@@ -1,5 +1,5 @@
 <?php
-require_once(__DIR__.'/../frood/src/Frood.php');
+require_once(__DIR__ . '/../lib/frood/src/Frood.php');
 $frood = new Frood('Lolmodule', 'public');
 //optional config here $frood->configure();
 $frood->dispatch();
